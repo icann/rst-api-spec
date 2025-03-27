5554f4164bcdc9f2c28d8101ed89c1a1a72cec30
Gavin Brown <gavin.brown@icann.org>
2025-03-27 09:48:33 +0000

    use the most recent release tag as version. run the rst-api-spec.html file through gpp, and sync YAML/JSON files from the releases page on GitHub rather than building them.

--- bin/build-pages.sh
+++ bin/build-pages.sh
@@ -4,7 +4,11 @@ SITEDIR="_site"
 rm -rf "$SITEDIR"
 mkdir -p "$SITEDIR/etc"
 
-gpp etc/index.md | pandoc \
+git config --global --add safe.directory /app
+
+CURRENT_RELEASE="$(git tag | tail -1)"
+
+gpp -x "-DRELEASE=$CURRENT_RELEASE" etc/index.md | pandoc \
     --from markdown \
     --to html \
     --standalone \
@@ -13,9 +17,12 @@ gpp etc/index.md | pandoc \
     --css=etc/style.css \
     --output="$SITEDIR/index.html"
 
-make spec
+gpp "-DRELEASE=$CURRENT_RELEASE" etc/rst-api-spec.html \
+    > "$SITEDIR/rst-api-spec.html"
 
 cp etc/*.svg "$SITEDIR/etc/"
-cp tmp/rst-api-spec.yaml "$SITEDIR/"
-cp tmp/rst-api-spec.json "$SITEDIR/"
-cp etc/rst-api-spec.html "$SITEDIR/"
+
+git tag | grep "^v" | while read RELEASE ; do
+    curl --silent --fail --location --output "$SITEDIR/rst-api-spec-$RELEASE.yaml" "https://github.com/icann/rst-api-spec/releases/download/$RELEASE/rst-api-spec.yaml"
+    curl --silent --fail --location --output "$SITEDIR/rst-api-spec-$RELEASE.json" "https://github.com/icann/rst-api-spec/releases/download/$RELEASE/rst-api-spec.json"
+done
