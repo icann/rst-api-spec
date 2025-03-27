3c9c5b6b6e761bdc20ec7415ab21f2c2d0f39336
Gavin Brown <gavin.brown@icann.org>
2025-03-27 10:28:32 +0000

    use self-hosted Swagger UI

--- bin/build-pages.sh
+++ bin/build-pages.sh
@@ -8,6 +8,9 @@ git config --global --add safe.directory /app
 
 CURRENT_RELEASE="$(git tag | tail -1)"
 
+echo "Current release is $CURRENT_RELEASE"
+
+echo "Creating index file..."
 gpp -x "-DRELEASE=$CURRENT_RELEASE" etc/index.md | pandoc \
     --from markdown \
     --to html \
@@ -17,10 +20,31 @@ gpp -x "-DRELEASE=$CURRENT_RELEASE" etc/index.md | pandoc \
     --css=etc/style.css \
     --output="$SITEDIR/index.html"
 
-gpp "-DRELEASE=$CURRENT_RELEASE" etc/rst-api-spec.html \
-    > "$SITEDIR/rst-api-spec.html"
-
+echo "Syncing spec files..."
 git tag | grep "^v" | while read RELEASE ; do
     curl --silent --fail --location --output "$SITEDIR/rst-api-spec-$RELEASE.yaml" "https://github.com/icann/rst-api-spec/releases/download/$RELEASE/rst-api-spec.yaml"
     curl --silent --fail --location --output "$SITEDIR/rst-api-spec-$RELEASE.json" "https://github.com/icann/rst-api-spec/releases/download/$RELEASE/rst-api-spec.json"
 done
+
+echo "Creating Swagger UI file..."
+gpp "-DRELEASE=$CURRENT_RELEASE" etc/rst-api-spec.html \
+    > "$SITEDIR/rst-api-spec.html"
+
+echo "Installing Swagger UI libraries..."
+SWAGGER_TMPDIR="_swagger_ui_tmp"
+rm -rf "$SWAGGER_TMPDIR"
+mkdir "$SWAGGER_TMPDIR"
+
+SWAGGER_UI_TAR=$(curl --silent --fail https://api.github.com/repos/swagger-api/swagger-ui/releases/latest | jq -r .tarball_url)
+
+curl --silent --location --fail --output "$SWAGGER_TMPDIR/swagger.tgz" "$SWAGGER_UI_TAR"
+
+cd "$SWAGGER_TMPDIR"
+tar zx --strip-components 1 -f swagger.tgz
+cd ..
+
+mv "$SWAGGER_TMPDIR/dist" "$SITEDIR/swagger-ui"
+
+rm -rf "$SWAGGER_TMPDIR"
+
+echo "done"
